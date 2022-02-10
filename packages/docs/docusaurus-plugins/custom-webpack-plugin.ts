2695d956dd764c9a9991ab13d62d19abe1690087
Denys Dovhan <denysdovhan@gmail.com>
2022-02-10 13:30:25 +0200

    Drop obsolete stylable loaders

--- packages/docs/docusaurus-plugins/custom-webpack-plugin.ts
+++ packages/docs/docusaurus-plugins/custom-webpack-plugin.ts
@@ -1,30 +1,5 @@
 const path = require('path');
 
-const stylableLoaders = [
-    {
-        loader: 'style-loader'
-    },
-    {
-        loader: 'css-loader',
-        options: {
-            importLoaders: 1,
-            modules: {
-                localIdentName: '[name]__[local]___[hash:base64:5]',
-                exportLocalsConvention: 'camelCase'
-            }
-        }
-    },
-    {
-        loader: 'resolve-url-loader'
-    },
-    {
-        loader: 'sass-loader',
-        options: {
-            sourceMap: true
-        }
-    }
-];
-
 module.exports = function() {
     return {
         name: 'custom-webpack-plugin',
@@ -42,14 +17,6 @@ module.exports = function() {
                             resourceQuery: /^\?raw-loader/,
                             use: ['raw-loader'],
                             type: 'javascript/auto'
-                        },
-                        {
-                            test: /\.st\.css$/,
-                            use: stylableLoaders
-                        },
-                        {
-                            test: /\.s[ac]ss$/i,
-                            use: stylableLoaders
                         }
                     ]
                 }
