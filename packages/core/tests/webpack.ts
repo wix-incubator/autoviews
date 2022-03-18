6380719932db99fe64ecb986cd8b2725ff887560
Viktor Havrylin <viktorh@wix.com>
2022-03-18 16:42:09 +0200

    Cleanup comment (#6)

--- packages/core/tests/webpack.ts
+++
@@ -1,4 +0,0 @@
-// https://webpack.js.org/guides/dependency-management/#require-context
-// the following is used an an entry point for karma (that runs karma-webpack)
-const contextModule = require.context('./', true, /.+\.spec\.ts[x]?$/);
-contextModule.keys().forEach(contextModule);
