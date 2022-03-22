e08a03862384506f30003f6e07e7fdf5ecca3932
Denys Dovhan <denysdovhan@gmail.com>
2022-03-22 20:25:52 +0200

    build: setup jest projects

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    projects: [
+        'packages/*/jest.config.js'
+    ],
+    testMatch: ['**/*.test.ts?(x)'],
+    globals: {
+        'ts-jest': {
+            diagnostics: {
+                warnOnly: true
+            }
+        }
+    }
+};
